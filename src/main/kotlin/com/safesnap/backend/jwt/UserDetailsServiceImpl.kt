370c222b7e9ffec66a2fc60e1e76002bef7cca3b
ahmedchowdhury <ahmedssofa@gmail.com>
2025-07-14 21:38:44 -0700

    Phase1 Added Cucumber test and unit test

--- src/main/kotlin/com/safesnap/backend/jwt/UserDetailsServiceImpl.kt
+++ src/main/kotlin/com/safesnap/backend/jwt/UserDetailsServiceImpl.kt
@@ -4,12 +4,13 @@ import com.safesnap.backend.repository.UserRepository
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
 class UserDetailsServiceImpl(private val userRepository: UserRepository) : UserDetailsService {
     override fun loadUserByUsername(email: String): UserDetails {
-        val user = userRepository.findByEmail(email) ?: throw RuntimeException("User not found")
+        val user = userRepository.findByEmail(email) ?: throw UsernameNotFoundException("User not found: $email")
         return org.springframework.security.core.userdetails.User(
             user.email,
             user.password,
